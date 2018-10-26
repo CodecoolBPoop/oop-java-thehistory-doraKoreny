d001e6991480717c3f75816506e89395119b773e
doraKoreny <dorakoreny@gmail.com>
2018-10-26 10:28:01 +0200

    Function size is implemented

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -49,7 +49,11 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        int countWords = 0;
+        for(String item : wordsArray) {
+            countWords++;
+        }
+        return countWords;
     }
 
     @Override
